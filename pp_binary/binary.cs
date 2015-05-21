15949316affa15198d72f295af9b7d2853a1e61c
krzysztofchadynka <kchadynka@o2.pl>
2015-05-21 07:39:01 +0200

    Create class binary

---
+++ pp_binary/binary.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Binary___pp
+{
+    class binary
+    {
+        public binary()
+        {
+
+        }
+
+        public long fromDecimal()
+        {
+            return 0;
+        }
+
+        public double toDecimal()
+        {
+            return 0;
+        }
+
+        // TODO: przeciazyc operatory +, -, *, /
+        //public operator + (binary val1, binary val2)
+        //{
+        //    return val1;
+        //}
+    }
+}
